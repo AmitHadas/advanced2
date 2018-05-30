09052e930d346b10d84707da93d0c8bfc13d6cce
מחשב <@DESKTOP-E5FKQN2>
2018-05-30 22:01:12 +0300

    added config

---
+++ ImageServiceWeb/Communication/ClientSingleton.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+using ImageService.Modal;
+using Newtonsoft.Json;
+
+
+namespace ImageServiceWeb.Communication
+{
+    class ClientSingleton
+    {
+
+
+
+        private TcpClient m_client;
+        /// <summary>
+        /// The m is listening
+        /// </summary>
+        private bool m_isListening;
+        /// <summary>
+        /// Delegate UpdateAfterResponse
+        /// </summary>
+        /// <param name="e">The <see cref="CommandRecievedEventArgs"/> instance containing the event data.</param>
+        public delegate void UpdateAfterResponse(CommandRecievedEventArgs e);
+        public event UpdateAfterResponse UpdateResponse;
+        /// <summary>
+        /// The m client instance
+        /// </summary>
+        private static ClientSingleton m_clientInstance;
+        /// <summary>
+        /// The m MTX
+        /// </summary>
+        private static Mutex m_mtx = new Mutex();
+        /// <summary>
+        /// The m is connected
+        /// </summary>
+        private bool m_isConnected;
+        /// <summary>
+        /// The stream
+        /// </summary>
+        /// 
+
+        private string m_isConnectedStr;
+
+        private NetworkStream stream;
+        /// <summary>
+        /// The reader
+        /// </summary>
+        private StreamReader reader;
+        /// <summary>
+        /// The writer
+        /// </summary>
+        private StreamWriter writer;
+        /// <summary>
+        /// The MTX
+        /// </summary>
+        private Mutex mtx;
+        /// <summary>
+        /// Gets a value indicating whether this instance is connected.
+        /// </summary>
+        /// <value><c>true</c> if this instance is connected; otherwise, <c>false</c>.</value>
+        /// 
+
+        public string IsConnectedStr
+        {
+            get
+            {
+                if (IsConnected)
+                {
+                    return "yes";
+                }
+                return "no";
+            }
+            set { }
+        }
+        public bool IsConnected { get; private set; }
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="GuiClientSingleton"/> class from being created.
+        /// </summary>
+        private ClientSingleton()
+        {
+            this.mtx = new Mutex();
+            this.IsConnected = this.Start();
+        }
+        /// <summary>
+        /// Gets the client insatnce.
+        /// </summary>
+        /// <value>The client insatnce.</value>
+        public static ClientSingleton ClientInsatnce
+        {
+            get
+            {
+                if (m_clientInstance == null)
+                {
+                    m_clientInstance = new ClientSingleton();
+                }
+                return m_clientInstance;
+            }
+        }
+
+        /// <summary>
+        /// Starts this instance.
+        /// </summary>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        private bool Start()
+        {
+            try
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3000);
+                this.m_client = new TcpClient();
+                this.m_client.Connect(endPoint);
+                stream = m_client.GetStream();
+                reader = new StreamReader(stream);
+                writer = new StreamWriter(stream);
+                Console.WriteLine("Client Connected");
+                m_isListening = true;
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Sends the command.
+        /// </summary>
+        /// <param name="e">The <see cref="CommandRecievedEventArgs"/> instance containing the event data.</param>
+        public void SendCommand(CommandRecievedEventArgs e)
+        {
+            new Task(() =>
+            {
+                try
+                {
+                    string commandToJson = JsonConvert.SerializeObject(e);
+                    //sending data to server
+                    Console.WriteLine($"Sending {commandToJson} to server");
+                    mtx.WaitOne();
+                    writer.WriteLine(commandToJson);
+                    writer.Flush();
+                    mtx.ReleaseMutex();
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.ToString());
+                }
+            }).Start();
+
+        }
+
+        /// <summary>
+        /// Receiveds the command.
+        /// </summary>
+        public void ReceivedCommand()
+        {
+            new Task(() =>
+            {
+                try
+                {
+                    while (m_isListening)
+                    {
+                        //Thread.Sleep(1000);
+                        //mtx.WaitOne();
+                        string responseString = reader.ReadLine();
+                        while (reader.Peek() > 0)
+                        {
+                            responseString += reader.ReadLine();
+                        }
+                        //mtx.ReleaseMutex();
+                        if (responseString != "")
+                        {
+                            //    responseString.Replace("#", Environment.NewLine);
+                            Console.WriteLine($"received {responseString} from Server");
+                            CommandRecievedEventArgs responseCommand =
+                                    JsonConvert.DeserializeObject<CommandRecievedEventArgs>(responseString);
+                            this.UpdateResponse?.Invoke(responseCommand);
+                        }
+                    }
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine(exp.ToString());
+                }
+            }).Start();
+        }
+
+    }
+}
+
+
+
+
--- ImageServiceWeb/Controllers/ProductsController.cs
+++ ImageServiceWeb/Controllers/ProductsController.cs
@@ -1,24 +1,39 @@
 ﻿using ImageServiceWeb.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Hosting;
 using System.Web.Mvc;
+using ImageServiceWeb.Communication;
+using ImageService.Modal;
+using ImageService.Infrastructure.Enums;
+using System.Collections.ObjectModel;
+using System.Threading;
 
 namespace ImageServiceWeb.Controllers
 {
     public class ProductsController : Controller
     {
+
+        static ClientSingleton client = ClientSingleton.ClientInsatnce;
         static string[] s1 = ReadFromFile(1);
         static string[] s2 = ReadFromFile(2);
-        static bool isServiceRunning = false;
-        static int numOfPictures = 100;
-        static List<Student> students = new List<Student>()
+        static string m_isServiceRunning = client.IsConnectedStr;
+        static int m_numOfPictures = 100;
+        static List<Student> m_students = new List<Student>()
         {
           new Student  { FirstName = s1[0] , LastName =  s1[1], ID = s1[2] },
           new Student  { FirstName = s2[0] , LastName =  s2[1], ID = s2[2] }
         };
+        static AppConfig config;
+        
+        public struct AppConfig
+        {
+            public string OutputDir;
+            public string SourceName;
+            public string LogName;
+            public string ThumbSize;
+            public ObservableCollection<string> handlers;
+            public bool isReady;
+        }
 
         // GET: Products
         public ActionResult Index()
@@ -28,11 +43,11 @@ namespace ImageServiceWeb.Controllers
         public ActionResult ImageWeb()
         {
             Data data = new Data();
-            data.students = students;
-            data.numOfPictures = numOfPictures;
-            data.isServiceRunning = isServiceRunning;
+            data.students = m_students;
+            data.numOfPictures = m_numOfPictures;
+            data.isServiceRunning = m_isServiceRunning;
 
-            return View(students);
+            return View(data);
         }
         public static string[] ReadFromFile(int line)
         {
@@ -45,8 +60,40 @@ namespace ImageServiceWeb.Controllers
         public struct Data
         {
             public List<Student> students;
-            public bool isServiceRunning;
+            public string isServiceRunning;
             public int numOfPictures;
+        }
+
+        public ActionResult Config()
+        {
+            client.UpdateResponse += GetConfig;
+            client.ReceivedCommand();
+            string[] args = { };
+            client.SendCommand(new CommandRecievedEventArgs((int)CommandEnum.GetConfigCommand, args, ""));
+            Thread.Sleep(1000);
+            return View(config);
+        }
+
+        public void GetConfig(CommandRecievedEventArgs e)
+        {
+               if(e.CommandID == (int)CommandEnum.GetConfigCommand)
+            {
+                string[] args = e.Args;
+                config = new AppConfig();
+
+                config.OutputDir = args[0];
+                config.SourceName = args[1];
+                config.LogName = args[2];
+                config.ThumbSize = args[3];
+
+                string[] directories = args[4].Split(';');
+                config.handlers = new ObservableCollection<string>();
+                foreach (var dir in directories)
+                {
+                    config.handlers.Add(dir);
+                }
+                config.isReady = true;
+            }
         }
     }
 }
