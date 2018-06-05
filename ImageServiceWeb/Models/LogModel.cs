0455f980f9ca4745cd8d11fffb22411900300279
noa or <noaor7@gmail.com>
2018-06-05 16:59:03 +0300

    added log not working

---
+++ ImageServiceWeb/Models/LogModel.cs
@@ -0,0 +1,39 @@
+﻿using ImageService.Infrastructure.Enums;
+using ImageService.Logging;
+using ImageService.Modal;
+using ImageServiceWeb.Communication;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace ImageServiceWeb.Models
+{
+    public class LogModel
+    {
+    
+        public ObservableCollection<LogEntry> Logs { get; }
+       
+
+        public ObservableCollection<Tuple<string, string>> LogsToShow { get; set; }
+
+        public string FilterType { get; set; }
+        public LogModel()
+        {
+            Logs = new ObservableCollection<LogEntry>();
+            LogsToShow = new ObservableCollection<Tuple<string, string>>();
+            FilterType = "";
+            ClientSingleton client = ClientSingleton.ClientInsatnce;
+            string[] args = { };
+            CommandRecievedEventArgs e = new CommandRecievedEventArgs((int)CommandEnum.GetLogList, args, "");
+            client.SendCommand(e);
+          //  Thread.Sleep(1000);
+
+        }
+
+
+
+    }
+}
